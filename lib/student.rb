b101fa1c743dba6f34ab61dcfab2c108bd043090
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-06-11 21:36:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,8 +11,7 @@ class Student
   def self.create_table
     sql = <<-SQL 
     CREATE TABLE IF NOT EXISTS students(
-    id 
-    INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     name TEXT,
     grade INTEGER)  
     SQL 
