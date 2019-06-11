d7dc8bd55198bf9ab72e24bad1ccec3e387432a3
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-06-11 21:17:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  def intializez(name, grade, id=nil)
+  def intialize(name, grade, id=nil)
     @name = name 
     @grade = grade
   end
