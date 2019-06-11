217cb222808e8866160a331844caeda9d68b000f
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-06-11 21:34:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,9 @@ class Student
   attr_reader :id 
   def self.create_table
     sql = <<-SQL 
-    CREATE TABLE IF NOT EXISTS students(id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)  
+    CREATE TABLE IF NOT EXISTS students(id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER)  
     SQL 
     DB[:conn].execute(sql)
   end 
